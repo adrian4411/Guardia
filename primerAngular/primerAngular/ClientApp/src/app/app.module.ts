9e7b2c408dd274b1d51f9f54f6400596781ea95a
adrian4411 <76117841+adrian4411@users.noreply.github.com>
2021-06-29 20:32:02 -0300

    2
    2

--- primerAngular/primerAngular/ClientApp/src/app/app.module.ts
+++ primerAngular/primerAngular/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { navMenuHorizontal } from './nav-menu-horizontal/nav-menu-horizontal.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    navMenuHorizontal
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
