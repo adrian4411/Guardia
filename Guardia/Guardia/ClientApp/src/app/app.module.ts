1992bd0ca9e479fe83b25d954b68fdf3cfb3b051
adrian4411 <76117841+adrian4411@users.noreply.github.com>
2021-07-13 18:51:12 -0300

    v0.0.02
    Creada una base

--- Guardia/Guardia/ClientApp/src/app/app.module.ts
+++ Guardia/Guardia/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { menuLateral } from './menu-lateral/menuLateral.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ShippingComponent,
+    menuLateral
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ Guardia/sada/ClientApp/src/app/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home2',
+  templateUrl: './home.component.html',
+})
+export class PruebaComponent {
+}
