7885c4df0cd4d2a9af16a83761291f294fd81dc7
adrian4411 <76117841+adrian4411@users.noreply.github.com>
2021-07-15 12:13:36 -0300

    V0.0.03
    Cambios en diseño CSS

--- Guardia/Guardia/ClientApp/src/app/menu-lateral/menuLateral.component.ts
+++ Guardia/Guardia/ClientApp/src/app/menu-lateral/menuLateral.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'menu-lateral',
-  templateUrl:'./menuLateral.component.html'
+  templateUrl: './menuLateral.component.html',
+  styleUrls: ['./menuLateral.component.css']
 })
 
 export class menuLateral{
